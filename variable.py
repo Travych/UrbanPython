37781f0b5b36a1b957e3354837c15ce609010534
Evgeniy <167197526+Travych@users.noreply.github.com>
2024-08-05 15:51:44 +0300

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,6 @@
+homework = 12
+hour = 1.5
+course_name = 'Python'
+time_work = (hour / homework )
+print("Курс:", course_name, ", всего задач:", homework, ",затарчено часов:", hour, ",среднее время выполнения", time_work,
+      "часа")
